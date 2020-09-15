5b89b9893e36d20ec4178f3c3abc61369e7c0790
Belle Sugi Zafimarovololona <sugi.zaf@onja.org>
2020-09-15 17:26:32 +0300

    Added modification

--- script.js
+++ script.js
@@ -3,40 +3,46 @@
 const dataurl = "./people.json";
 const tbody = document.querySelector('tbody');
 const container = document.querySelector('.tablebody');
-const response = fetch(dataurl);
+// const response = fetch(dataurl);
 
-console.log(response);
+
+// console.log(response);
 
 // Fetch 
 async function fetchPeople() {
-const response = await fetch(dataurl);
-const data = await response.json();
-return data;
+  const response = await fetch(dataurl);
+  console.log(response);
+  const data = await response.json();
+    return data;
 
 
 }
 
 fetchPeople();
 
-
 // Create html
 
 async function displayDatalist() {
     const persons = await fetchPeople();
 
-    const html = persons.map(person => {
+    // Sort the movie
+
+    const sortePeople = persons.sort((a, b) => {
+      return b.birthday - a.birthday;
+    })
+
+    const html = sortePeople.map(person => {
        return `
-    <tr data-id="${person.id}" class="">
+    <tr data-id="${person.id}" class="row">
     <td><img src="${person.picture}" alt="${person.firstName + ' ' + person.lastName}"/></td>
-    <td>${person.id}</td>
     <td>${person.lastName}</td>
     <td>${person.firstName}</td>
     <td>${person.birthday}</td>
     <td class = "icons">
-      <button class="edit">
+      <button class="edit" id="${person.id}">
         <img class = "icons__image" src="assets/icon_edit.png" alt="edit">
       </button>
-      <button class="delete">
+      <button class="delete" id="${person.id}">
         <img class = "icons__image" src="assets/delete.png" alt="delete">
       </button>
     </td>
@@ -53,30 +59,69 @@ async function displayDatalist() {
 
 displayDatalist();
 
-const editPerson = e => {
+// Editing the person's birhtday
+
+// async function editPerson (e) {
+  
+// }
+
+async function editPersonpopup(id) {
+  const response = await fetch(dataurl);
+  const data = await response.json();
+  // console.log(data);
+  const result = data.find(person => person.id === id);
+  console.log(result);
+  const popup = document.createElement('form');
+  popup.classList.add('.popupedit');
+  popup.insertAdjacentHTML('afterbegin', `
+  <fieldset style="border: none;">
+    <label for="${result.lastName}">LastName</label><br>
+    <input type="text" value="${result.lastName}" id="${result.lastName}">
+  </fieldset>
+  <fieldset style="border: none;">
+    <label for="${result.firstNam}">FisrtName</label><br>
+    <input type="text" value="${result.firstName}" id="${result.firstNam}">
+  </fieldset style="border: none;">
+  <fieldset style="border: none;">
+    <label for="${result.birthday}">Birthday</label><br>
+    <input type="text" value="${result.birthday}" id="${result.birthday}">
+  </fieldset>
+  <div class="button-sub">
+    <button class="button__save">Save</button>
+    <button class="button__cancel">Cancel</button>
+  </div>
+  `).join('');
+
+  document.body.appendChild(popup);
+  popup.classList.add('open');
+  console.log(popup)
+}
+
+// Deleting the birthday
+
+async function deletePerson (e) {
 
 }
 
+// Event listner function
 
-const ediPerson = id => {
-  const same = data.find(person => person.id === id);
-
-  console.log(same)
-}
-
-
-
-// Editing function
-
-const handleClick = e => {
-    const editButton = e.target.closest('.edit');
-    const deleteButton = e.target.closest('.delete');
+async function handleClick (e){
+    const editButton = e.target.matches('.edit');
+   const deletButton = e.target.matches('.delete');
+  
 
     if(editButton) {
+      const id = editButton.id;
+      editPersonpopup(id);
+    }
+   
+    if(deletButton) {
+      const id = deletButton.id;
 
     }
 
-    if (deleteButton) {
-
-    }
 }
+
+window.addEventListener('click', handleClick);
+
+// tbody.addEventListener('click', editPerson)
