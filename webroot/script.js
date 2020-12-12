93fd0fd1ee1f145a2e71a86cf7efe8f7bd81c579
Belle Sugi Zafimarovololona <sugi.zaf@onja.org>
2020-12-12 11:35:56 +0300

    Deleted the function of the month and adding emojy

--- webroot/script.js
+++ webroot/script.js
@@ -1,74 +1,65 @@
-
 //  Grabe some element I might need
 
-  const dataurl = "./people.json";
-  const tbody = document.querySelector('tbody');
-  const container = document.querySelector('.tablebody');
-  const addButon = document.querySelector('.add-people');
-  const input = document.getElementById("filter_user"); 
+const dataurl = "./people.json";
+const tbody = document.querySelector("tbody");
+const container = document.querySelector(".tablebody");
+const addButon = document.querySelector(".add-people");
+const input = document.getElementById("filter_user");
 
 //  Create an empty array to store the people.json
 
-  let peopleStore = [];
+let peopleStore = [];
 
+//  Fetch the file in the people.json
+async function fetchPeople() {
+  const response = await fetch(dataurl);
+  const data = await response.json();
+  peopleStore = [...data];
+  displayDatalist(peopleStore);
 
-//  Fetch the file in the people.json 
-  async function fetchPeople() {
-    const response = await fetch(dataurl);
-    const data = await response.json();
-    peopleStore = [...data];
-    displayDatalist(peopleStore);
-    
-    return data;
-
+  return data;
 }
 
-  fetchPeople();
+fetchPeople();
 
-
-  async function displayDatalist(peopleStore) {
-
-    const html = peopleStore
-      .map((person) => {
-
-
-    function nthDate(days) {
-      if (days > 3 && days < 21) 
-      return "th";
-      switch (days % 10) {
-        case 1: return "st";
-        case 2: return "nd";
-        case 3: return "rd";
-        default: return "th"
+async function displayDatalist(peopleStore) {
+  const html = peopleStore
+    .map((person) => {
+      function nthDate(days) {
+        if (days > 3 && days < 21) return "th";
+        switch (days % 10) {
+          case 1:
+            return "st";
+          case 2:
+            return "nd";
+          case 3:
+            return "rd";
+          default:
+            return "th";
+        }
       }
-    }
-        
-    const DateNow = new Date(person.birthday);
-    const Now = new Date();
-    const month = DateNow.getMonth();
-    const Nowday = DateNow.getDate();
-    const year = DateNow.getFullYear();
+
+      const DateNow = new Date(person.birthday);
+      const Now = new Date();
+      const month = DateNow.getMonth();
+      const Nowday = DateNow.getDate();
+      const year = DateNow.getFullYear();
+
+      const yearForNow = Now.getFullYear();
+      const birthdayTime = new Date(yearForNow, month, Nowday);
+      const aday = 1000 * 60 * 60 * 24;
+      const RealDate = birthdayTime.getTime() - Now.getTime();
+      const MoreDay = Math.ceil(RealDate / aday);
+
+      const Alldate = `${DateNow}${nthDate(DateNow)} / ${month + 1} / ${year}`;
 
 
-    const yearForNow = Now.getFullYear();
-    const birthdayTime = new Date(yearForNow, month, Nowday);
-    const aday = 1000 * 60 * 60 * 24;
-    const RealDate = birthdayTime.getTime() - Now.getTime();
-    const MoreDay = Math.ceil(RealDate / aday);
-
-     const Alldate = `${DateNow}${nthDate(DateNow)} / ${month + 1} / ${year}`;
-    //  const futureDate = DateNow.getFullYear() - year;
-
-    //  const months = {
-      
-    //  }
-
-        return `
+      return `
           <tr data-id= "${person.id}" class="row row-container">
             <td>
           <img src="${person.picture}" alt="${
-          person.firstName + " " + person.lastName
-        }"/>
+        person.firstName + " " + person.lastName
+      }"/>
         </td>
           <td>
                 ${person.lastName} - ${person.firstName}
@@ -94,25 +85,25 @@
           </td>
         </tr>
   `;
-      })
-      .join("");
+    })
+    .join("");
 
-    tbody.innerHTML = html;
-
+  tbody.innerHTML = html;
 }
 
-  displayDatalist();
+displayDatalist();
 
 //  Calculate the date of the birthday
 
 //  Popup for editing people
 
-    const editPersonpopup = async function(id) {
-
-      const result = peopleStore.find(person => person.id === id);
-      const popup = document.createElement('form');
-      popup.classList.add('popupedit');
-      popup.insertAdjacentHTML('afterbegin', `
+const editPersonpopup = async function (id) {
+  const result = peopleStore.find((person) => person.id === id);
+  const popup = document.createElement("form");
+  popup.classList.add("popupedit");
+  popup.insertAdjacentHTML(
+    "afterbegin",
+    `
         <fieldset style="border: none;">
           <label for="picture">Url image</label><br>
           <input type="url" value="${result.picture}" id="picture">
@@ -133,112 +124,100 @@
           <button class="button__save" type="submit">Save</button>
           <button class="button__cancel" type="button">Cancel</button>
         </div>
-        `);
+        `
+  );
 
-      document.body.appendChild(popup);
-      popup.classList.add('open');
+  document.body.appendChild(popup);
+  popup.classList.add("open");
 
+  //  Add event listener for the edit and save or not save the change
 
-//  Add event listener for the edit and save or not save the change
-
-
-  popup.addEventListener('submit', e => {
-
+  popup.addEventListener("submit", (e) => {
     const saveChange = e.currentTarget;
     e.preventDefault();
 
-      result.picture = popup.picture.value;
-      result.lastName = popup.lastname.value;
-      result.firstName = popup.firstname.value;
-      result.birthday = popup.birthday.value;
-      result.id = id;
-      displayDatalist(peopleStore);
-      console.log(birthday);
-      saveChange;
-      const remove = (popup.style.display = "none");
-      remove;
-       tbody.dispatchEvent(new CustomEvent("itemUpdated"));
-
-
- });
-
-//  Close the popup when the user does not want the change they have made in the person's information
-
-  popup.addEventListener('click', e => {
-    const cancelTheChange = e.target.closest('.button__cancel');
-    if (cancelTheChange) {
-      const remove = popup.style.display = 'none';
-      remove;
-    }
-
+    result.picture = popup.picture.value;
+    result.lastName = popup.lastname.value;
+    result.firstName = popup.firstname.value;
+    result.birthday = popup.birthday.value;
+    result.id = id;
+    displayDatalist(peopleStore);
+    console.log(birthday);
+    saveChange;
+    const remove = (popup.style.display = "none");
+    remove;
+    tbody.dispatchEvent(new CustomEvent("itemUpdated"));
   });
 
-}
+  //  Close the popup when the user does not want the change they have made in the person's information
+
+  popup.addEventListener("click", (e) => {
+    const cancelTheChange = e.target.closest(".button__cancel");
+    if (cancelTheChange) {
+      const remove = (popup.style.display = "none");
+      remove;
+    }
+  });
+};
 
 //  Function for deleting the birthday
 
-    async function deletePersonPopup(id) {
-
-      const deleteOne = peopleStore.find(person => person.id === id);
-      console.log(deleteOne);
-      const popup = document.createElement('article');
-      popup.classList.add('.confirm');
-      popup.insertAdjacentHTML('afterbegin', `
+async function deletePersonPopup(id) {
+  const deleteOne = peopleStore.find((person) => person.id === id);
+  console.log(deleteOne);
+  const popup = document.createElement("article");
+  popup.classList.add(".confirm");
+  popup.insertAdjacentHTML(
+    "afterbegin",
+    `
         <p class="deleteparagraph">
           Are you sure you want to delete this person
         </p>
         <div class="container__buttom">
           <button class="confirm_buttom yes__sure"> Yes </button>
           <button class="confirm_buttom no__want"> No </button>
-        </div>`);
+        </div>`
+  );
 
-        document.body.appendChild(popup);
-        popup.classList.add('.confirm');
+  document.body.appendChild(popup);
+  popup.classList.add(".confirm");
 
-//  Fuction to confirm the deletion of the birthday
+  //  Fuction to confirm the deletion of the birthday
 
-    const confirm = async function(e) {
+  const confirm = async function (e) {
+    //  Grabing all the button
 
-//  Grabing all the button
+    const removeEl = e.target.closest(".yes__sure");
+    const no = e.target.closest(".no__want");
 
-    const removeEl = e.target.closest('.yes__sure');
-    const no = e.target.closest('.no__want');
- 
-//  Condition for all the button
+    //  Condition for all the button
 
-    if(removeEl) {
+    if (removeEl) {
       e.preventDefault();
-      const deletePeople = peopleStore.filter(person => person.id !== id);
+      const deletePeople = peopleStore.filter((person) => person.id !== id);
       peopleStore = deletePeople;
       console.log(deletePeople);
       displayDatalist(deletePeople);
       const changeRemove = (popup.style.display = "none");
       changeRemove;
-
-    }
-
-    else if (no) {
-      const remove = popup.style.display = 'none';
+    } else if (no) {
+      const remove = (popup.style.display = "none");
       remove;
-
     }
+  };
 
-  }
-
-  window.addEventListener('click', confirm);
-   tbody.dispatchEvent(new CustomEvent("itemUpdated"));
-
+  window.addEventListener("click", confirm);
+  tbody.dispatchEvent(new CustomEvent("itemUpdated"));
 }
 
 //  Adding the list
 
-    addButon.addEventListener('click', function addNewPeople() {
-  
-      const popup = document.createElement('form');
-      popup.classList.add('popupadd');
-      popup.insertAdjacentHTML(
-        "afterbegin",
-        `
+addButon.addEventListener("click", function addNewPeople() {
+  const popup = document.createElement("form");
+  popup.classList.add("popupadd");
+  popup.insertAdjacentHTML(
+    "afterbegin",
+    `
         <fieldset style="border: none;">
           <label for="picture">Url image</label><br>
           <input type="url" value="" id="picture" required>
@@ -260,21 +239,20 @@
           <button class="close" type="button"> Close </button>
         </div>
   `
-      );
-  
-      document.body.appendChild(popup);
-      popup.classList.add('add');
+  );
 
-  popup.addEventListener('submit', e => {
+  document.body.appendChild(popup);
+  popup.classList.add("add");
+
+  popup.addEventListener("submit", (e) => {
     e.preventDefault();
     const addNewOne = e.currentTarget;
     const newList = {
       id: addNewOne.id,
-      picture : addNewOne.picture.value,
-      lastName : addNewOne.lastname.value,
-      firstName : addNewOne.firstname.value,
-      birthday : addNewOne.birthday.value,
-      
+      picture: addNewOne.picture.value,
+      lastName: addNewOne.lastname.value,
+      firstName: addNewOne.firstname.value,
+      birthday: addNewOne.birthday.value,
     };
 
     console.log(addNewOne);
@@ -282,74 +260,54 @@
     displayDatalist(peopleStore);
     addNewOne.reset();
     addNewPeople();
-     tbody.dispatchEvent(new CustomEvent("itemUpdated"));
+    tbody.dispatchEvent(new CustomEvent("itemUpdated"));
     //  editPersonpopup(result);
-    
-  
   });
 
-  // Close the popup when the user does need to add a new person 
+  // Close the popup when the user does need to add a new person
 
-  popup.addEventListener('click', e => {
-    const cancelTheChange = e.target.closest('.close');
-    if(cancelTheChange) {
-    const remove = popup.style.display = 'none';
-    remove;
+  popup.addEventListener("click", (e) => {
+    const cancelTheChange = e.target.closest(".close");
+    if (cancelTheChange) {
+      const remove = (popup.style.display = "none");
+      remove;
     }
-
   });
-
- 
 });
-
 
 //  Local storage function
 
 const recordeLocalStorage = () => {
-  const isItems = JSON.parse(localStorage.getItem('peopleStore'));
-  if(isItems) {
+  const isItems = JSON.parse(localStorage.getItem("peopleStore"));
+  if (isItems) {
     peopleStore = isItems;
     displayDatalist();
   }
-   tbody.dispatchEvent(new CustomEvent('itemUpdated'))
-
- 
- 
-
-}
-
+  tbody.dispatchEvent(new CustomEvent("itemUpdated"));
+};
 
 // restoreLocalStorage();
 
-  function restoreLocalStorage() {
-
+function restoreLocalStorage() {
   localStorage.setItem("peopleStore", JSON.stringify(peopleStore));
-
 }
-
 
 // Event listner function
 
-async function handleClick(e){
-    const editButton = e.target.closest('.edit');
-    const deletButton = e.target.closest('.delete');
+async function handleClick(e) {
+  const editButton = e.target.closest(".edit");
+  const deletButton = e.target.closest(".delete");
 
-    if(editButton) {
-      const id = e.target.closest(".row-container").dataset.id;
-      editPersonpopup(id);
-    
-    }
-   
-    if(deletButton) {
-      const id = deletButton.id;
-      deletePersonPopup(id);
+  if (editButton) {
+    const id = e.target.closest(".row-container").dataset.id;
+    editPersonpopup(id);
+  }
 
-    }
-
+  if (deletButton) {
+    const id = deletButton.id;
+    deletePersonPopup(id);
+  }
 }
 
-  window.addEventListener('click', handleClick);
-  tbody.addEventListener('itemUpdated', restoreLocalStorage);
-  
- 
-
+window.addEventListener("click", handleClick);
+tbody.addEventListener("itemUpdated", restoreLocalStorage);
